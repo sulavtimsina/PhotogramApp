a685a724879f07ffb05b8e5c0d338b7b8889caa7
Sulav Timsina <sulavtimsina@gmail.com>
2018-04-09 15:11:55 -0500

    end of class commit

--- app/src/main/java/com/example/sulav/constraintlayouttry/adapters/RecyclerAdapter.java
+++ app/src/main/java/com/example/sulav/constraintlayouttry/adapters/RecyclerAdapter.java
@@ -1,21 +1,17 @@
 package com.example.sulav.constraintlayouttry.adapters;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.sulav.constraintlayouttry.Model.Post;
 import com.example.sulav.constraintlayouttry.R;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import java.util.ArrayList;
 
@@ -30,11 +26,16 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     }
 
     // create a new view
+    // Here we attach
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        /* = R.layout.row_item = layout of each row item
+           = parent
+           = false*/
         View v= layoutInflater.inflate(R.layout.row_item, parent, false);
+        /*Pass this view to our MyViewHolder constructor and return it*/
         return new MyViewHolder(v);
 
     }
@@ -43,8 +44,11 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         // - get element from your dataset at this position
-        // - replace the contents of the view with that element
+        // - post is the Post object at the 'position', so we receive it and use it to populate
+        // - view components using Post's fields.
         Post post = posts.get(position);
+        //here holder is MyViewHolder object, its tvName is the text view which we have referred in
+        // MyViewHolder class below. so using this below line, we can set text in that TextView.
         holder.tvName.setText(post.getName());
         holder.tvEmail.setText(post.getEmail());
 
@@ -52,12 +56,15 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
         imageLoader.displayImage(post.getPhoto_location(), holder.ivProfilePic);
     }
 
+    //return the size of arraylist or array
     @Override
     public int getItemCount() {
         return posts.size();
     }
 
-
+    //This is the class which holds each row item.
+    // give reference to all the view components which has to be accessed or modified in runtime
+    // like we have to setText in tvName.
     public class MyViewHolder extends RecyclerView.ViewHolder  {
         TextView tvName, tvEmail;
         ImageView ivProfilePic;
---
+++ app/src/main/java/com/example/sulav/constraintlayouttry/retrofit/RetrofitClient.java
@@ -0,0 +1,4 @@
+package com.example.sulav.constraintlayouttry.retrofit;
+
+public class RetrofitClient {
+}
