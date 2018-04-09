098531c8fda5075dc813ef61fb3125b42d19cf2e
Sulav Timsina <sulavtimsina@gmail.com>
2018-04-09 15:12:47 -0500

    end of class commit with all files added

--- app/src/main/java/com/example/sulav/constraintlayouttry/retrofit/ApiInterface.java
+++ app/src/main/java/com/example/sulav/constraintlayouttry/retrofit/ApiInterface.java
@@ -1,4 +1,14 @@
 package com.example.sulav.constraintlayouttry.retrofit;
 
+import com.example.sulav.constraintlayouttry.Model.Post;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+
 public interface ApiInterface {
+
+    @POST("echoJson.php")
+    Call<List<Post>> getPosts();
 }
