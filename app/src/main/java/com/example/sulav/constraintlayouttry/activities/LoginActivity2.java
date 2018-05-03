3797cb0bb70a5d92d56fe22569598647da2f3bc4
Sulav Timsina <sulavtimsina@gmail.com>
2018-05-02 19:47:56 -0500

    project submission

---
+++ app/src/main/java/com/example/sulav/constraintlayouttry/activities/LoginActivity2.java
@@ -0,0 +1,13 @@
+package com.example.sulav.constraintlayouttry.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
